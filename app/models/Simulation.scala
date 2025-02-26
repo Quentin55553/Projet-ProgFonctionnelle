3ba2d25f9ced951ab758445e58aea5a4ba096980
axel <axel.malherbe@etu.cyu.fr>
2025-02-26 16:55:16 +0100

    a

---
+++ app/models/Simulation.scala
@@ -0,0 +1,21 @@
+package models
+
+class Simulation {
+
+  def BuyRSI(): Unit = {
+
+  }
+  def SellRSI(): Unit = {
+
+  }
+  def BuyMACD(): Unit = {
+
+  }
+  def SellMACD(): Unit = {
+
+  }
+
+  def updatePortfolio(): Unit = {
+
+  }
+}
