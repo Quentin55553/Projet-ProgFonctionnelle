94673c74e17eb718be5061a8392f7aa33a97b797
Quentin55553 <67703561+Quentin55553@users.noreply.github.com>
2025-03-03 20:14:00 +0100

    Ajout dépendances/fichier pour API

---
+++ app/api/APIHandler.scala
@@ -0,0 +1,16 @@
+package api
+
+
+class APIHandler {
+  private val baseUrl = "https://finnhub.io/api/v1"
+  private val apiKey = "cv2vkohr01qk43tvjktgcv2vkohr01qk43tvjku0"
+
+
+  // Il faut réussir à importer les modules permettant de faire des appels à l'API
+
+  // À faire :
+  // - Méthode pour récupérer le prix des actions (peut-être faire plusieurs méthodes avec des paramètres différents)
+  // - Méthode pour récupérer le prix des cryptos (à exécuter car normalement payant)(si c'est bien payant, on génère les valeurs aléatoirement)
+  // - Méthode pour récupérer le prix des devises
+  // - Méthodes pour récupérer les mouvements récents les plus importants des marchés
+}
--- build.sbt
+++ build.sbt
@@ -5,9 +5,12 @@ ThisBuild / version := "1.0-SNAPSHOT"
 lazy val root = (project in file("."))
   .enablePlugins(PlayScala)
   .settings(
-    name := """Akka""",
+    name := "Akka",
     libraryDependencies ++= Seq(
       guice,
-      "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
+      "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1" % Test,
+      "com.softwaremill.sttp.client3" %% "core" % "3.10.1",
+      "com.softwaremill.sttp.client3" %% "akka-http-backend" % "3.10.1",
+      "com.typesafe.akka" %% "akka-stream" % "2.8.7"
     )
   )
