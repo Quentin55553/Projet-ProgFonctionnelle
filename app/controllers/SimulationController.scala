cd58fcc4f2abe6c141de0622c90234f16d5cd869
axel <axel.malherbe@etu.cyu.fr>
2025-03-18 10:43:41 +0100

    a

--- app/controllers/SimulationController.scala
+++ app/controllers/SimulationController.scala
@@ -7,18 +7,18 @@ object SimulationController extends App {
 
   val fromDate = LocalDate.of(2025, 2, 14)
   val toDate = LocalDate.of(2025, 3, 14)
-  val prices = DataFetcher.fetchHistoricalPrices("AAPL", fromDate, toDate)
+  val pricesDates = DataFetcher.fetchHistoricalPrices("AAPL", fromDate, toDate)
 
   val riskFreeRate = 0.01
-  val simulation = new Simulation(prices, riskFreeRate)
+  val simulation = new Simulation(pricesDates, riskFreeRate)
 
   println("Prix historiques récupérés :")
-  prices.foreach { priceDate =>
+  pricesDates.foreach { priceDate =>
     println(s"${priceDate.date} : prix = ${priceDate.price} €")
   }
   println()
 
-  val lastDate = prices.last.date
+  val lastDate = pricesDates.last.date
 
   println("Entrez une date pour l'évaluation (au format YYYY-MM-DD) ou tapez 'exit' pour quitter :")
   var inputDate = scala.io.StdIn.readLine()
@@ -28,8 +28,8 @@ object SimulationController extends App {
       val dateToEvaluate = LocalDate.parse(inputDate)
 
       if (dateToEvaluate.isAfter(lastDate)) {
-        val selectedPrices = prices.map(_.price)
-        val prevision = new Prevision(selectedPrices)
+        val prices = pricesDates.map(_.price)
+        val prevision = new Prevision(prices)
 
         val futureDays = (dateToEvaluate.toEpochDay - lastDate.toEpochDay).toInt
 
@@ -43,20 +43,7 @@ object SimulationController extends App {
         println(s"1. Prix prédit (Régression Linéaire): $predictedPriceRegression €")
         println(s"2. Prix prédit (Moyenne Mobile - 7 jours): $predictedPriceMA €")
       } else {
-        prices.find(_.date == dateToEvaluate) match {
-          case Some(priceDate) =>
-            val selectedPrices = prices.takeWhile(_.date.isBefore(dateToEvaluate.plusDays(1))).map(_.price)
-            val financialMetrics = FinancialMetrics(selectedPrices, riskFreeRate)
-
-            println(s"\nÉvaluation pour la date: $dateToEvaluate, prix: ${priceDate.price} €")
-            println(simulation.evaluateRSI(selectedPrices))
-            println(simulation.evaluateMACD(selectedPrices))
-            println(s"Volatilité: ${financialMetrics.volatility().formatted("%.4f")}")
-            println(s"Ratio de Sharpe: ${financialMetrics.sharpeRatio().formatted("%.4f")}")
-
-          case None =>
-            println(s"Aucun prix trouvé pour la date: $dateToEvaluate")
-        }
+        simulation.evaluateIndicatorsForDate(dateToEvaluate)
       }
     } catch {
       case e: Exception => println("Date invalide. Veuillez entrer une date correcte au format YYYY-MM-DD.")
